e658bf53e9554395a775b5249ea7eb3a83d9c874
Franco <131843647+FrancoAK07@users.noreply.github.com>
2025-05-13 20:32:54 -0400

    updated espermatograma & urocultivo results

--- client/src/views/UrocultivoResults.vue
+++ client/src/views/UrocultivoResults.vue
@@ -24,7 +24,7 @@
 								<div class="d-inline fw-bold">CI:</div>
 								{{ profile?.ci }}
 							</div>
-							<div v-if="profile?.doctor" class="col">
+							<div v-show="profile?.doctor" class="col">
 								<div class="d-inline fw-bold">Dr:</div>
 								{{ profile?.doctor }}
 							</div>
@@ -299,6 +299,7 @@
 		germs.value = await store.fecthBacterium();
 		antibioticos.value = await store.fecthAntibiotics();
 		result.value = await store.fetchCultiveResult(profile.value.orders[0].idOrder, profileNames.value);
+		console.log(result.value);
 		resultData.value = result.value.resultado;
 		resultSensibles.value = result.value.sensibles;
 		resultResistentes.value = result.value.resistentes;
@@ -423,7 +424,7 @@
 
 		const element = html;
 
-		const html2pdf = (await import('html2pdf.js')).default;
+		const html2pdf = (await import("html2pdf.js")).default;
 
 		if (!print.value) {
 			html2pdf().from(element).set(options).save();
@@ -446,7 +447,6 @@
 			});
 		}
 	};
-
 	const generatePDF: any = async () => {
 		html = headerPatientInfo.value.innerHTML;
 		const bacteriologicoCopy = bacteriologico.value.cloneNode(true);
@@ -530,7 +530,7 @@
 
 		const element = html;
 
-		const html2pdf = (await import('html2pdf.js')).default;
+		const html2pdf = (await import("html2pdf.js")).default;
 
 		if (!print.value) {
 			html2pdf().from(element).set(options).save();
@@ -560,22 +560,28 @@
 		const bacteriologicoInputs = bacteriologicoCopy.querySelectorAll("input");
 		const contaje = bacteriologicoInputs[0].value;
 		const observaciones = bacteriologicoInputs[1].value;
-		const sensiblesValues: string[] = [];
-		const resistentesValues: string[] = [];
-		const sensiblesSelects = sensibles.value.querySelectorAll("select");
-		const resistentesSelects = resistentes.value.querySelectorAll("select");
+		const sensiblesValues: string[] = Array.from(sensibles.value.querySelectorAll("select"))
+			.map((sel: any) => sel.value)
+			.filter((val) => val);
+		const resistentesValues: string[] = Array.from(resistentes.value.querySelectorAll("select"))
+			.map((sel: any) => sel.value)
+			.filter((val) => val);
+		// const sensiblesValues: string[] = [];
+		// const resistentesValues: string[] = [];
+		// const sensiblesSelects = sensibles.value.querySelectorAll("select");
+		// const resistentesSelects = resistentes.value.querySelectorAll("select");
 
-		sensiblesSelects.forEach((item: any) => {
-			if (item.value) {
-				sensiblesValues.push(item.value);
-			}
-		});
+		// sensiblesSelects.forEach((item: any) => {
+		// 	if (item.value) {
+		// 		sensiblesValues.push(item.value);
+		// 	}
+		// });
 
-		resistentesSelects.forEach((item: any) => {
-			if (item.value) {
-				resistentesValues.push(item.value);
-			}
-		});
+		// resistentesSelects.forEach((item: any) => {
+		// 	if (item.value) {
+		// 		resistentesValues.push(item.value);
+		// 	}
+		// });
 
 		const data = {
 			idOrder: profile.value.orders[0].idOrder,
@@ -615,7 +621,7 @@
 		};
 		await ordersStore.updateStatusOrder(profileId, data);
 		await generatePDFWithoutSignature();
-		const message = `Adjuntos resultados del laboratorio`
+		const message = `Adjuntos resultados del laboratorio`;
 		if (!profile.value.phone || profile.value.phone === "" || profile.value.phone === undefined || profile.value.phone === null) {
 			const whatsappUrl = `https://web.whatsapp.com/send`;
 			window.open(whatsappUrl, "_blank");
@@ -682,7 +688,7 @@
 			jsPDF: { unit: "mm", format: "letter", orientation: "portrait" },
 		};
 
-		const html2pdf = (await import('html2pdf.js')).default;
+		const html2pdf = (await import("html2pdf.js")).default;
 
 		html2pdf().from(element).set(options).save();
 	};
