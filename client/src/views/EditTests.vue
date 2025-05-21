4b60164b2dbdfa8518ff861dc239a726a5f0ab4d
Franco <131843647+FrancoAK07@users.noreply.github.com>
2025-05-20 22:41:22 -0400

    updated addresults & fixed empty section

--- client/src/views/EditTests.vue
+++ client/src/views/EditTests.vue
@@ -661,7 +661,7 @@
 							seccion.camposAgregados.push(...idsCorrespondientes);
 							seccion.camposAgregados = seccion.camposAgregados.filter((id) => id !== 0);
 
-							if (seccion.idDivision !== 0){
+							if (seccion.idDivision !== 0) {
 								await tests.createProfileSectionInputs(data, seccion.camposAgregados);
 							}
 						}
@@ -694,9 +694,9 @@
 					});
 					camposExistentes.value = await tests.fecthProfilesInputs();
 					unidadesDeCampos.value = await tests.fecthProfilesInputUnits();
-					seccionesActualizadas.length = 0
-					seccionesAgregadas.value = []
-					seccionesEliminadas.value = []
+					seccionesActualizadas.length = 0;
+					seccionesAgregadas.value = [];
+					seccionesEliminadas.value = [];
 				}
 			}
 		}
@@ -964,7 +964,9 @@
 
 		secciones.value.push(nuevaSeccion);
 
-		if (!seccionesAgregadas.value.includes(nuevaSeccion)) {
+		const seccionRepetida = seccionesAgregadas.value.find((item) => item.nombre.trim() === nuevaSeccion.nombre.trim());
+
+		if (!seccionRepetida && nuevaSeccion.nombre && nuevaSeccion.camposAgregados.length > 0) {
 			seccionesAgregadas.value.push(nuevaSeccion);
 		}
 	};
